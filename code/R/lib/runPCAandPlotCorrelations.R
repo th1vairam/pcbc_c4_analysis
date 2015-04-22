63e1320f997e96e2ae25f198de3d41b037846bb8
th1vairam <th.vairam@gmail.com>
2015-04-22 01:33:12 +0000

    Some minor changes for code readability

--- code/R/lib/runPCAandPlotCorrelations.R
+++ code/R/lib/runPCAandPlotCorrelations.R
@@ -1,13 +1,13 @@
-source('../R/lib/runPCA.R')
-source('../R/lib/calcCompleteCorAndPlot.R')
 # Function to run principal component analysis and plot correlations
 runPCAandPlotCorrelations <- function(genesBySamples, samplesByCovariates, dataName, isKeyPlot=FALSE, 
                                       SCALE_DATA_FOR_PCA = TRUE, MIN_PVE_PCT_PC = 1.0, CORRELATION_TYPE = "pearson",
-                                      ALSO_PLOT_ALL_COVARS_VS_PCA = TRUE, MAX_NUM_LEVELS_PER_COVAR = 10) {
+                                      ALSO_PLOT_ALL_COVARS_VS_PCA = TRUE, MAX_NUM_LEVELS_PER_COVAR = 50) {
+      
   title = paste(ifelse(SCALE_DATA_FOR_PCA, "S", "Un-s"), "caled ", dataName, " ", " data in PCA; PVE >= ", MIN_PVE_PCT_PC, "%; ", CORRELATION_TYPE, " correlations ", sep="")
   writeLines(paste("\nRunning PCA and calculating correlations for:\n", title, sep=""))
  
-  pcaRes <- runPCA(genesBySamples=genesBySamples, SCALE_DATA_FOR_PCA=SCALE_DATA_FOR_PCA, 
+  pcaRes <- runPCA(genesBySamples=genesBySamples, 
+                   SCALE_DATA_FOR_PCA=SCALE_DATA_FOR_PCA, 
                    MIN_PVE_PCT_PC=MIN_PVE_PCT_PC)
   
   samplePCvals <- pcaRes$samplePCvals
@@ -29,7 +29,13 @@ runPCAandPlotCorrelations <- function(genesBySamples, samplesByCovariates, dataN
   if (ALSO_PLOT_ALL_COVARS_VS_PCA) { LOOP_PLOT_ALL_COVARS = unique(c(LOOP_PLOT_ALL_COVARS, TRUE)) }
   
   for (PLOT_ALL_COVARS in LOOP_PLOT_ALL_COVARS) {
-    corrRes = calcCompleteCorAndPlot(samplePCvals, samplesByCovariates, CORRELATION_TYPE, title, PLOT_ALL_COVARS, EXCLUDE_VARS_FROM_FDR)
+    corrRes = calcCompleteCorAndPlot(samplePCvals, 
+                                     samplesByCovariates, 
+                                     CORRELATION_TYPE, 
+                                     title, 
+                                     WEIGHTS = pve[1:dim(samplePCvals)[2]],
+                                     PLOT_ALL_COVARS, 
+                                     EXCLUDE_VARS_FROM_FDR)
     add_PC_res[[length(add_PC_res)+1]] = list(plotData=corrRes$plot, isKeyPlot=(isKeyPlot && !PLOT_ALL_COVARS))
     if (!PLOT_ALL_COVARS) {
       significantCovars = corrRes$significantCovars
