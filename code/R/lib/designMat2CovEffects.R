b6d4202082db3062a5aba088bfd20385db4aaea9
th1vairam <th.vairam@gmail.com>
2015-05-22 16:43:44 +0000

    Modified designMat2CovEffects to give average effects and not the summed effects

--- code/R/lib/designMat2CovEffects.R
+++ code/R/lib/designMat2CovEffects.R
@@ -1,3 +1,3 @@
 designMat2CovEffects <-function(COVARIATES_MAP, sigCovars.Effects){
-  sapply(COVARIATES_MAP,function(x,y = sigCovars.Effects){return(sum(y[x]))})
+  sapply(COVARIATES_MAP,function(x,y = sigCovars.Effects){return(mean(y[x]))})
 }
